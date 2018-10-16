2bf194bea1fbb393dea8c6bb046fbde6fb60d51c
HWHuangeian <18795991898@163.com>
2018-10-16 20:32:24 +0800

    commit test

--- src/main/java/com/huangweihan/xweb/controller/UserController.java
+++ src/main/java/com/huangweihan/xweb/controller/UserController.java
@@ -1,15 +1,26 @@
 package com.huangweihan.xweb.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+/**
+ * Description
+ *
+ * @author Administrator
+ * @date 2018-10-16
+ */
 @Controller
 public class UserController {
+
+    Logger log = LoggerFactory.getLogger(UserController.class);
 
     @ResponseBody
     @RequestMapping("/login")
     public String hello() {
+        log.info("hello() {}", "coming");
         return "hello";
     }
 
