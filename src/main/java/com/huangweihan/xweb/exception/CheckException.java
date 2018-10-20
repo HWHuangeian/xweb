75a0e1df544205c2839a3f4ad8fd4edb559e4ff1
HWHuangeian <18795991898@163.com>
2018-10-20 16:38:47 +0800

    commit test

--- src/main/java/com/huangweihan/xweb/exception/CheckException.java
+++ src/main/java/com/huangweihan/xweb/exception/CheckException.java
@@ -7,4 +7,21 @@ package com.huangweihan.xweb.exception;
  * @date: 2018/10/18 0018
  */
 public class CheckException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public CheckException() {
+    }
+
+    public CheckException(String message) {
+        super(message);
+    }
+
+    public CheckException(Throwable cause) {
+        super(cause);
+    }
+
+    public CheckException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
