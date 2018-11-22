7899b23e761da62718fbb5c5e67b3ad3d649103c
HWHuangeian <18795991898@163.com>
2018-11-23 00:07:49 +0800

    commit test

---
+++ src/main/java/com/huangweihan/xweb/core/utils/CollectionUtil.java
@@ -0,0 +1,39 @@
+package com.huangweihan.xweb.core.utils;
+
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * Description
+ *
+ * @author: Administrator
+ * @date: 2018/11/23 0023
+ */
+public class CollectionUtil {
+
+    /**
+     * 集合工具类是否为null或空
+     * @param collection
+     * @return
+     */
+    public static boolean isEmpty(Collection<?> collection) {
+        return collection == null || collection.size() == 0;
+    }
+
+    public static <K, V extends Comparable<? super V>> Map<K, V> sortByValue(Map<K, V> map, int flag) {
+        Map<K, V> sortMap = new LinkedHashMap<>();
+        if(flag == 1) {
+            map.entrySet().stream()
+                    .sorted((o1, o2) -> o1.getValue().compareTo(o2.getValue()))
+                    .forEach(entry -> sortMap.put(entry.getKey(), entry.getValue()));
+        } else {
+            map.entrySet().stream()
+                    .sorted((o1, o2) -> o2.getValue().compareTo(o1.getValue()))
+                    .forEach(entry -> sortMap.put(entry.getKey(), entry.getValue()));
+        }
+        return sortMap;
+    }
+
+
+}
---
+++ src/main/java/com/huangweihan/xweb/core/utils/StringUtil.java
@@ -0,0 +1,10 @@
+package com.huangweihan.xweb.core.utils;
+
+/**
+ * Description
+ *
+ * @author: Administrator
+ * @date: 2018/11/23 0023
+ */
+public class StringUtil {
+}
